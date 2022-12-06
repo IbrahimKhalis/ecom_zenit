d18f01b05196a3b1a8e0695b8fab1a2c90073e4d
DivadanAr <divadan.arya12@gmail.com>
2022-12-06 12:48:15 +0700

    page edit information

--- resources/views/seller/setting-store.blade.php
+++ resources/views/seller/setting-store.blade.php
@@ -14,7 +14,54 @@
         <a href="{{ route('setting-scedhule') }}">Scedhule</a>
     </div>
     
-    <div class="card-store"></div>
+    <div class="card-store">
+      <div class="header-cards">
+        <i class="fa-solid fa-store"></i>
+        <p>Store Information</p>
+      </div>
+      <div class="card-content">
+        <div class="row1">
+          <div class="col1">
+            <div class="store-name">
+              <label for="name">Store Name</label>
+              <input type="text" name="name" id="name" value="Agus Mart">
+            </div>
+            <div class="desc">
+              <label for="desc">Description</label>
+              <textarea name="" id="" cols="30" rows="6"></textarea>
+            </div>    
+          </div>
+          <div class="col2">
+            <div class="store-profile">
+              <p>Store Profile</p>
+              <div class="input-file"></div>
+            </div>
+          </div>
+        </div>
+        <div class="row2">
+          <p>Social Media Stores</p>
+          <div class="content-row2">
+            <div class="col1">
+              <div class="instagram">
+                <label for="ig"><iconify-icon icon="mdi:instagram"></iconify-icon> Instagram</label>
+                <input type="text" name="ig" id="ig">
+              </div>  
+              <div class="gmail">
+                <label for="gmail"><img src="{{ asset('assets/img/gmail.png') }}" alt=""> Gmail</label>
+                <input type="text" name="ig" id="ig">
+              </div>  
+            </div>
+            <div class="col2">
+              <div class="linkedin">
+                <label for="ig"><img src="{{ asset('assets/img/linkedin.png') }}" alt=""> linkedin</label>
+                <input type="text" name="ig" id="ig">
+              </div>  
+            </div>
+  
+          </div>
+        </div>
+      </div>
+    </div>
  
 </div>
 
